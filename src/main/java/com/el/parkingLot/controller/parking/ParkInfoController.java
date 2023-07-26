f7ba1d91c546a8099205506f29a914027fbda5ef
nownuu98 <nownuu98@naver.com>
2023-07-26 09:26:06 +0900

    주차장 정보 보기 구현 중 (전으로 돌리기...)

---
+++ src/main/java/com/el/parkingLot/controller/parking/ParkInfoController.java
@@ -0,0 +1,32 @@
+package com.el.parkingLot.controller.parking;
+
+import com.el.parkingLot.dto.parking.ParkLocaDto;
+import com.el.parkingLot.dto.parking.ParkInfoDto;
+import com.el.parkingLot.service.parking.ParkInfoService;
+import com.el.parkingLot.service.parking.ParkLocaService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class ParkInfoController {
+
+    private final ParkInfoService parkInfoService;
+    private final ParkLocaService parkLocaService;
+
+    @PostMapping
+    public ResponseEntity<Void> saveParkInfo(@RequestBody ParkInfoDto parkInfoDto) {
+        parkInfoService.saveParkInfo(parkInfoDto);
+        return ResponseEntity.ok().build();
+    }
+
+    // 내 차량 주차장 정보 보기
+    @GetMapping("/parkingLot/parkLocaInfo/{carNum}") // 경로 변수로 지정
+    public ResponseEntity<List<ParkLocaDto>> getMyParkingInfo(@PathVariable String carNum) {
+        List<ParkLocaDto> parkLocaList = parkLocaService.findByCarNum(carNum);
+        return ResponseEntity.ok(parkLocaList);
+    }
+}
